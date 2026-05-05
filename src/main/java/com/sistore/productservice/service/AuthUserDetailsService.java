903a5286b7f360273fabf8a59f5f7af408da5cbe
Swaraj <sahooswaraj582@gmail.com>
2026-05-06 00:54:48 +0530

    Fix service layer security error.

--- src/main/java/com/sistore/productservice/service/AuthUserDetailsService.java
+++ src/main/java/com/sistore/productservice/service/AuthUserDetailsService.java
@@ -1,17 +1,23 @@
 package com.sistore.productservice.service;
 
+import com.sistore.productservice.entity.AuthUser;
+import com.sistore.productservice.repository.AuthUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthUserDetailsService implements UserDetailsService {
     @Autowired
-    private UserRepository userRepository;
+    private AuthUserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username)
             throws UsernameNotFoundException {
 
-        User user = userRepository.findByUsername(username)
+        AuthUser user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return org.springframework.security.core.userdetails.User
